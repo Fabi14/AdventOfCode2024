cf1a29f1d7add91f3f9bab6cec2a8f15f1a7c24c
Fabi14 <fabian22191@gmail.com>
2024-12-09 20:07:29 +0100

    Day 5 part 2

--- Day05/aoc2024_day05/aoc2024_day05.cpp
+++ Day05/aoc2024_day05/aoc2024_day05.cpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 int main()
 {
@@ -16,7 +17,9 @@ int main()
     std::ifstream input("input.txt");
 
     std::map<int, std::vector<int>> rules;
+    std::map<int, std::vector<int>> rulesForFixing;
     int sum{ 0 };
+    std::vector<std::vector<int>> incorrectlyOrderedUpdates{};
     for (std::string line; std::getline(input, line);)
     {
         if (readInRules)
@@ -40,6 +43,7 @@ int main()
                 |  std::ranges::to <std::vector<int>>() ;
             if (pair.size() == 2) {
                 rules[pair[1]].push_back(pair[0]);
+                rulesForFixing[pair[0]].push_back(pair[1]);
             }
         }
         else
@@ -71,8 +75,22 @@ int main()
             {
                 sum += update[update.size() / 2];
             }
+            else
+            {
+                incorrectlyOrderedUpdates.push_back(update);
+            }
         }
     }
 
-    std::cout << sum;
+    std::cout << sum << '\n';
+    int sum2{ 0 };
+    for (auto& u : incorrectlyOrderedUpdates)
+    {
+        std::ranges::partial_sort(u,u.begin()+u.size()/2+1, [&](int a, int b)
+            {
+                return std::ranges::find(rulesForFixing[a], b) != rulesForFixing[a].end();
+            });
+        sum2 += u[u.size() / 2];
+    }
+    std::cout << sum2;
 }
